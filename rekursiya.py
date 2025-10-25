b18c11e58bee970badfab4e85809425da5030c8c
Yarashov 201017 <yarashovulugbek8@gmail.com>
2025-10-25 16:24:01 +0500

    lomda added

--- rekursiya.py
+++ rekursiya.py
@@ -1,7 +1,12 @@
-def sonni_teskarilash(son):
-    if son < 10 :
-        return son
-    else :
-        return son%10 + sonni_teskarilash(son//10)
+# def sonni_teskarilash(son):
+#     if son < 10 :
+#         return son
+#     else :
+#         return son%10 + sonni_teskarilash(son//10)
 
-print(sonni_teskarilash(132))
+# print(sonni_teskarilash(132))
+
+# Lambda
+
+son = lambda a: a%2 == 0 
+print(son(10))
